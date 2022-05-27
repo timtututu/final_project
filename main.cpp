98b1fbf6e6c616e8ba7350d95d485945270a0d92
timtim <pupss97306@gmail.com>
2022-05-28 00:53:31 +0800

    commit_4

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@
 #define CENTER_BASE 1500
  Ticker servo_ticker;
  Ticker encoder_ticker;
- PwmOut pin5(D5), pin6(D6);
+ PwmOut pin5(D5), pin6(D6);//pin6 servo1 正常
  BusInOut qti(D3,D4,D7,D8);
  //DigitalInOut pin10(D10);//ping
  DigitalIn encoder(D11);
@@ -27,13 +27,13 @@
  EventQueue queue_1(32 * EVENTS_EVENT_SIZE);
  parallax_ping  ping1(pin10);
  Timer timer;
- Thread t,t2,t3,t_1;
+ Thread t,t2,t3,t_1,t_2;
  //parallax_ping  ping1(pin10);
  float  time_pass=0,pdistant=0;
  double pingbef=0,pingminu=0,judge=1;
  double pingtemp[2];
  bool K=0,jud=1;
- int ok=1,timess=1,timess2=1,speed=0,qtirec=0,state=0;
+ int ok=1,timess=0,timess2=1,speed=0,qtirec=0,state=0;
  volatile int steps;
  volatile int last;
 /**
@@ -77,12 +77,10 @@ void encoder_control() {
    last = value;
 }
 void statusjudge(){
-    if(state==1) {
-       steps=0;
-       car.goStraight(20);
-       ThisThread::sleep_for(1000ms);
-       state=2;
-    } 
+        if(qtirec==0b1111&&timess==0){
+            state++;
+            timess=1;
+        }
 }
 void Qtirecord(){
     pdistant=ping1;
@@ -92,7 +90,7 @@ void Qtirecord(){
     qti.input();
     wait_us(250);
     qtirec=qti;
-    printf("%d ,%d\n",qtirec,state);
+   printf("%d ,%d,t=%d,%f\n",qtirec,state,timess,pdistant);
 }
 void suddenstop(){
         speed=0;
@@ -104,23 +102,69 @@ void suddenstop(){
 
 
 double facter=0.3;
-double speedt=50,speedt2=40,speedt3=60;
+double speedt=40,speedt2=40,speedt3=50;
 void Qtijudge(){
     
     car.stop();
-    if(qtirec==0b0110){
+    /*if(qtirec==0b0110){
         car.goStraight(-10);
         ThisThread::sleep_for(1ms);
-    }
+    }*/
     //ThisThread::sleep_for(1ms);
-    if(qtirec==0b1000) car.turn(speedt3,facter); //left
-    else if(qtirec==0b1100) car.turn(speedt,facter);
-    else if(qtirec==0b0100) car.turn(speedt2,facter);
-    else if(qtirec==0b0110) car.goStraight(18);
-    else if(qtirec==0b0010) car.turn(speedt2,-facter);//right
-    else if(qtirec==0b0011) car.turn(speedt,-facter);
-    else if(qtirec==0b0001) car.turn(speedt3,-facter);
-    else if(qtirec==0b0000) car.goStraight(-20);
+    if(qtirec==0b1000){
+        car.turn(speedt3,facter); //left
+        timess=0;
+        //state=0;
+    } 
+    else if(qtirec==0b1100){
+        car.turn(speedt2,facter);
+        timess=0;
+        //state=0;
+    }
+    else if(qtirec==0b0100){
+        car.turn(speedt,facter);
+        timess=0;
+        //state=0;
+    } 
+    else if(qtirec==0b0110){
+        car.goStraight(18.5);
+        timess=0;
+        //state=0;
+    } 
+    else if(qtirec==0b0010){
+        car.turn(speedt,-facter);//right
+        timess=0;
+        state=0;
+    } 
+    else if(qtirec==0b0011){
+        car.turn(speedt2,-facter);
+        timess=0;
+        //state=0;
+    } 
+    else if(qtirec==0b0001){
+        car.turn(speedt3,-facter);
+        timess=0;
+        //state=0;
+    }
+    else if(qtirec==0b0000){
+        car.goStraight(-20);
+        timess=0;
+        //state=0;
+    } 
+    /*else if(qtirec==0b1111&&state==0){
+         car.goStraight(20);
+    }*/
+    else if(qtirec==0b1111&&state==2){
+        //qtirec=0b1000;
+        //ThisThread::sleep_for(10ms);
+        car.turn(speedt3,facter);
+        ThisThread::sleep_for(5ms);
+    } 
+    else if(qtirec==0b1001||qtirec==0b1101||qtirec==0b1011){
+        car.goStraight(10);
+        //state=0;
+        timess=0;
+    } 
 }
 
 /** erpc infrastructure */
@@ -130,34 +174,14 @@ erpc::BasicCodecFactory basic_cf;
 erpc::Crc16 crc16;
 erpc::SimpleServer rpc_server;
 
-
-void erpc_call(){
-    rpc_server.run();
-}
 /** LED service */
 LEDBlinkService_service led_service;
-
-int main(void) {
-   double pwm_table0[] = {-150, -120, -90, -60, -30, 0, 30, 60, 90, 120, 150};
-   double speed_table0[] = {42.252,41.136,37.947,28.620,13.393,0.000,16.024,30.214,38.346,41.694,42.970};
-   double pwm_table1[] = {-150, -120, -90, -60, -30, 0, 30, 60, 90, 120, 150};
-   double speed_table1[] = {41.376,40.020,37.150,28.381,12.755,0.000,16.741,30.693,40.180,41.056};
-
-  // first and fourth argument : length of table
-  car.setCalibTable(11, pwm_table0, speed_table0, 11, pwm_table1, speed_table1);
-  //pin5.period_ms(20);
-  //pin6.period_ms(20);
-  t.start(callback(&queue, &EventQueue::dispatch_forever));
-  queue.call_every(10ms, Qtirecord);
-  t3.start(callback(&queue3, &EventQueue::dispatch_forever));
-  queue3.call_every(10ms, Qtijudge);
-  t2.start(callback(&queue2, &EventQueue::dispatch_forever));
-  queue2.call_every(10ms, statusjudge);
-
-
-
-
-  // Initialize the rpc server
+void erpc_call(){
+  
+    rpc_server.run();
+}
+void erpc_init(){
+        // Initialize the rpc server
   uart_transport.setCrc16(&crc16);
 
   // Set up hardware flow control, if needed
@@ -180,6 +204,36 @@ int main(void) {
   // Run the server. This should never exit
   printf("Running server.\n");
   //rpc_server.run();
+}
+
+
+int main(void) {
+ /* double pwm_table0[] = {150, 120, 90, 60, 30, 0, -30, -60, -90, -120, -150};
+   double speed_table0[] = {42.252,41.136,37.947,28.620,13.393,0.000,16.024,30.214,38.346,41.694,42.970};*/
+   double pwm_table0[] = {-150, -120, -90, -60, -30, 0, 30, 60, 90, 120, 150};
+   double speed_table0[] = {41.376,40.020,37.150,28.381,12.755,0.000,16.741,30.693,40.180,41.056};
+   double pwm_table1[] = {-150, -120, -90, -60, -30, 0, 30, 60, 90, 120, 150};
+   double speed_table1[] = {41.376,40.020,37.150,28.381,12.755,0.000,16.741,30.693,40.180,41.056};
+
+  // first and fourth argument : length of table
+  car.setCalibTable(11, pwm_table0, speed_table0, 11, pwm_table1, speed_table1);
+  //pin5.period_ms(20);
+  //pin6.period_ms(20);
+  t.start(callback(&queue, &EventQueue::dispatch_forever));
+  queue.call_every(1ms, Qtirecord);
+  t3.start(callback(&queue3, &EventQueue::dispatch_forever));
+  queue3.call_every(5ms, Qtijudge);
+  t2.start(callback(&queue2, &EventQueue::dispatch_forever));
+  queue2.call_every(5ms, statusjudge);
+  //t_2.start(erpc_init);
+  /*t_1.start(callback(&queue_1, &EventQueue::dispatch_forever));
+  queue_1.call_every(5ms,erpc_call);*/
+
+
+
+
+
+
 //*****erpc**********
 
 
