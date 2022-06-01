32269651b7dff086fbb88a447ad46ef3ae7cd451
timtim <pupss97306@gmail.com>
2022-06-01 18:45:25 +0800

    commit_9

--- main.cpp
+++ main.cpp
@@ -15,26 +15,26 @@
 #define CENTER_BASE 1500
  Ticker servo_ticker;
  Ticker encoder_ticker;
- PwmOut pin5(D5), pin6(D6);//pin6 servo1 正常
+ PwmOut pin5(D5), pin6(D6);//pin6 servo1 正常  右手馬達
  BusInOut qti(D3,D4,D7,D8);
  //DigitalInOut pin10(D10);//ping
  DigitalIn encoder(D11);
  DigitalInOut pin10(D10);
- BBCar car(pin5, pin6, servo_ticker);
+ BBCar car(pin5, pin6, servo_ticker);//p5 servo 0
  EventQueue queue(32 * EVENTS_EVENT_SIZE);
  EventQueue queue2(32 * EVENTS_EVENT_SIZE);
  EventQueue queue3(32 * EVENTS_EVENT_SIZE);
-  EventQueue queue_3(32 * EVENTS_EVENT_SIZE);
+ EventQueue queue_3(32 * EVENTS_EVENT_SIZE);
  EventQueue queue_1(32 * EVENTS_EVENT_SIZE);
  parallax_ping  ping1(pin10);
  Timer timer;
- Thread t,t2,t3,t_1,t_2,t_3;
+ Thread t,t2,t3,t_1,t_2,t_3,t_encoder;
  //parallax_ping  ping1(pin10);
- float  time_pass=0,pdistant=0;
+ float  time_pass=0,pdistant=0,speednow=0;
  double pingbef=0,pingminu=0,judge=1;
  double pingtemp[2];
  bool K=0,jud=1;
- int ok=1,timess=0,timesstemp=1,speed=0,qtirec=0,state=0,tempstate,obstacle=0,pingbeg=0;
+ int ok=1,timess=0,timesstemp=1,speed=0,qtirec=0,state=0,tempstate,obstacle=0,pingbeg=0,final=0;
  volatile int steps;
  volatile int last;
 /**
@@ -62,7 +62,7 @@ void stop(uint8_t car){
         // Uncomment for actual BB Car operations
         // (*cars[car -1]).stop();
         //printf("Car %d stop.\n", car);
-        printf("%d ,%d,t=%d,%f,obs=%d\n",qtirec,state,timess,pdistant,obstacle);
+        printf("%d ,%d,t=%d,%f,obs=%d,temp=%d,dis=%f,speed=%f\n",qtirec,state,timess,pdistant,obstacle,tempstate,steps*6.5*3.14/32,speednow);
   }
 }
 
@@ -72,7 +72,7 @@ void goStraight(uint8_t car, int32_t  speed){
         // Uncomment for actual BB Car operations
         // (*cars[car -1]).goStraight(speed);
         //printf("Car %d go straight at speed %d.\n", car, speed);
-        printf("%d ,%d,t=%d,%f,obs=%d\n",qtirec,state,timess,pdistant,obstacle);
+        printf("%d ,%d,t=%d,%f,obs=%d,temp=%d,dis=%f,speed=%f\n",qtirec,state,timess,pdistant,obstacle,tempstate,steps*6.5*3.14/32,speednow);
   }
 }
 
@@ -82,7 +82,7 @@ void turn(uint8_t car, int32_t speed, double factor){
         // Uncomment for actual BB Car operations
         // (*cars[car -1]).turn(speed, factor);
         //printf("Car %d turn at speed %d with a factor of %f.\n", car, speed, factor);
-        printf("%d ,%d,t=%d,%f,obs=%d\n",qtirec,state,timess,pdistant,obstacle);
+        printf("%d ,%d,t=%d,%f,obs=%d,temp=%d,dis=%f,speed=%f\n",qtirec,state,timess,pdistant,obstacle,tempstate,steps*6.5*3.14/32,speednow);
   }
 }
 /****** erpc  *******/
@@ -90,8 +90,15 @@ void turn(uint8_t car, int32_t speed, double factor){
 
 
 void encoder_control() {
+    timer.start();
    int value = encoder;
-   if (!last && value) steps++;
+   if (!last && value){
+       steps++;
+       timer.stop();
+       time_pass=timer.read();
+       speednow=1*6.5*3.14/32/time_pass;
+       timer.reset();
+    } 
    last = value;
 }
 int once=0;
@@ -145,20 +152,20 @@ void suddenstop(){
 
 
 double facter=0.4;
-double speedt=35,speedt2=40,speedt3=0;//25 38 38 18.5
-double straight=19.5;
+double speedt=35,speedt2=38,speedt3=38;//25 38 38 18.5
+double straight=19;
 void Qtijudge(){
     car.stop();
     if(qtirec==0b1000&&state!=-1){
-            car.turn(speedt3,facter); //right
+            car.turn(45,facter); //right
             timess=0;
     } 
     else if((qtirec==0b1100||qtirec==0b1110)&&state!=-1){
-            car.turn(speedt2,0.6); //right
+            car.turn(45,facter); //right
             timess=0;
     }
     else if(qtirec==0b0100&&state!=-1){
-            car.turn(speedt,facter); //right
+            car.turn(40,facter);//right
             timess=0;
     } 
     else if(qtirec==0b0110&&state!=-1){
@@ -193,17 +200,24 @@ void Qtijudge(){
 
     else if(qtirec==0b1111&&obstacle==1&&timess==0){
         car.turn(45,facter);
-        printf("in\n");
+        //printf("in\n");
         ThisThread::sleep_for(1s);//要得
         obstacle=2;
-        timess==1;
+        //timess=1;//未測試
         car.goStraight(straight);
         ThisThread::sleep_for(500ms);
     } //1111其一狀況 回頭  //以上未改
     else if(qtirec==0b1111&&obstacle==2&&timess==0){
-        car.turn(speedt3,facter);
-        ThisThread::sleep_for(1s);
-        timess==1;
+        car.turn(38,facter);
+        ThisThread::sleep_for(880ms);
+        car.goStraight(straight);
+        ThisThread::sleep_for(300ms);
+        //timess=1;//未測試
+        final++;
+        /*if(final>3){
+            car.turn(50,1);
+            ThisThread::sleep_for(1000ms);
+        }*/
     }
     else if((qtirec==0b1001||qtirec==0b1101||qtirec==0b1011||qtirec==0b0101||qtirec==0b1010)&&state!=-1){
             car.goStraight(straight);//left
@@ -212,8 +226,8 @@ void Qtijudge(){
     } //排除特例
     else if(state==-1){
         //printf("in\n");
-        car.turn(40,1);
-        ThisThread::sleep_for(270ms);//要得
+        car.turn(75,1);
+        ThisThread::sleep_for(350ms);//要得
         state=tempstate;
         obstacle=1;
         timess=1;
@@ -224,9 +238,11 @@ void pingprint(){
     pdistant=ping1;
 }
 void print(){
-    printf("%d ,%d,t=%d,%f,obs=%d,temp=%d\n",qtirec,state,timess,pdistant,obstacle,tempstate);
+    //printf("%d ,%d,t=%d,%f,obs=%d,temp=%d\n",qtirec,state,timess,pdistant,obstacle,tempstate);
 }
-
+void encoder_t(){
+    encoder_ticker.attach(&encoder_control, 1ms);
+}
 
 /** erpc infrastructure */
 ep::UARTTransport uart_transport(D1, D0, 9600);
@@ -283,7 +299,8 @@ int main(void) {
   queue3.call_every(2ms, Qtijudge);
   t2.start(callback(&queue2, &EventQueue::dispatch_forever));
   queue2.call_every(3ms, statusjudge);
-  //t_2.start(erpc_init);
+  t_2.start(erpc_init);
+  t_encoder.start(encoder_t);
   t_1.start(callback(&queue_1, &EventQueue::dispatch_forever));
   queue_1.call_every(700ms, pingprint);
   t_3.start(callback(&queue_3, &EventQueue::dispatch_forever));
